7482f7fc4d6838d6d6848fa457bdd43d2e66d044
joe_tian_forexco_home_macmini <j.tian@forexco.com.au>
2024-07-30 00:54:19 +1000

    fix:wip

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { z } from "zod";
+import { fromZodError } from "zod-validation-error";
 
 // Section 1: basic example
 enum Hobbies {
@@ -43,6 +44,27 @@ const UserSchema = z
 
 type UserSchemaType = z.infer<typeof UserSchema>;
 
+// Error handling Example:
+const demoTestUser: UserSchemaType = {
+  id: "123",
+  name: "John",
+  age: 13,
+  friends: null,
+  hobbies: [Hobbies.CODING, Hobbies.READING],
+  coords: [0, 0, 0],
+  random: Math.random(),
+  isNew: true,
+};
+
+const safeParseResult = UserSchema.safeParse(demoTestUser);
+
+if (safeParseResult.success) {
+  console.log(UserSchema.safeParse(demoTestUser).data);
+} else {
+  console.log('fromZodError =.= =.= =.=');
+  console.log(fromZodError(safeParseResult.error));
+}
+
 const UserPartialSchema = UserSchema.partial();
 
 type UserPartialSchemaType = z.infer<typeof UserPartialSchema>;
@@ -72,16 +94,72 @@ const getUserWithDefaults = (user: UserPartialSchemaType): UserSchemaType => {
 const userWithDefaults: UserSchemaType = getUserWithDefaults(apiUser); // Note: missing default values
 
 // Section 1 - Output
-console.log(
-  "checking UserSchema.safeParse(userWithDefaults).error --->",
-  UserSchema.safeParse(userWithDefaults).error
-);
-// console.log(UserSchema.safeParse(userWithDefaults).data);
+// console.log(
+//   "checking UserSchema.safeParse(userWithDefaults).error --->",
+//   UserSchema.safeParse(userWithDefaults).error
+// );
+
 
 // Section 2: Object Types
 
 // console.log("checking shape---->", UserSchema.shape.name);
 
 // Section 3: Record Types
-const demoMapSchema = z.record(z.string(), z.number());
+const demoRecordSchema = z.record(
+  z.string(),
+  z.object({ name: z.string(), age: z.number() })
+);
+type DemoRecordSchemaType = z.infer<typeof demoRecordSchema>;
+
+const demoMapSchema = z.map(
+  z.string(),
+  z.object({ name: z.string(), age: z.number() })
+);
 type DemoMapSchemaType = z.infer<typeof demoMapSchema>;
+
+const demoMapObj: DemoMapSchemaType = new Map([
+  ["id-john", { name: "John", age: 20 }],
+  ["id-doe", { name: "Doe", age: 30 }],
+]);
+
+// console.log("checking demoMapObj---->", demoMapObj);
+
+const demoSetSchema = z
+  .set(z.union([z.string(), z.number()]))
+  .min(2)
+  .max(3);
+type DemoSetSchemaType = z.infer<typeof demoSetSchema>;
+const validSet: DemoSetSchemaType = new Set(["a", "b", 1, 2]);
+// console.log(
+//   "checking demoSetSchema.safeParse(validSet).error --->",
+//   demoSetSchema.safeParse(validSet).error
+// );
+
+// Section 4: Promise Types
+const PromiseSchema = z.promise(
+  z.object({ id: z.union([z.string(), z.number()]), name: z.string() })
+);
+
+// const PromiseAwaitedSchema = z.await(PromiseSchema)
+
+type PromiseSchemaType = z.infer<typeof PromiseSchema>;
+
+const promiseDemoObj: PromiseSchemaType = Promise.resolve({
+  id: "123",
+  name: "John",
+});
+
+// Section 5: Custom Types
+const brandEmailSchema = z
+  .string()
+  .email()
+  .refine((val) => val.endsWith("@ptxmarkets.com"), {
+    message: "emaill need to end with @ptxmarkets.com",
+  });
+
+const testEmail = "ddd@ptxmarkets.com";
+
+// console.log(
+//   "checking brandEmailSchema.safeParse(testEmail).error --->",
+//   brandEmailSchema.safeParse(testEmail).error?.issues
+// );
